9ae51a301894aa824ebbb6586817ad33798d5be1
G3rard <aankop3n@gmail.com>
2015-06-09 00:03:02 +0200

    First commit

--- frontpage/sonos/config.php
+++ frontpage/sonos/config.php
@@ -2,10 +2,9 @@
 
 $config = array(
 	'zones' => array(
-                '22Kantoor'   => '192.168.1.161',
+                'Kantoor'   => '192.168.1.161',
 	),
 	'radiostations' => array(
-		/*'q'  => 'x-rincon-mp3radio://icecast-qmusic.cdp.triple-it.nl/Qmusic_nl_nonstop_96.mp3',*/
 		'DasDing'  => 'swr-mp3-m-dasding.akacast.akamaistream.net/7/588/137139/v1/gnl.akacast.akamaistream.net/swr-mp3-m-dasding',
         '1Live'    => '1live-diggi.akacast.akamaistream.net/7/965/119435/v1/gnl.akacast.akamaistream.net/1live-diggi',
 		'538'      => 'vip-icecast.538.lw.triple-it.nl/RADIO538_MP3',
@@ -15,6 +14,6 @@ $config = array(
         'messageLang'   	=> 'nl',
         'currentRadio'  	=> 'currentRadio.txt',
         'filePhpSonos'  	=> 'PHPSonos.inc.php',
-        'logging'       	=> true,
+        'logging'       	=> false,
         'logfile'       	=> 'log.txt',
 );
